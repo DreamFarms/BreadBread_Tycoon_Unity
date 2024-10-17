b8528721528b85c47d996ca33efa273d7bf565b2
Jihwan Byeon <wkdghk24@gmail.com>
2024-10-17 13:36:22 +0900

    fix : Add UI
    - UI 수정 및 추가

--- Assets/02_Scripts/Recipe/RecipeUIManager.cs
+++ Assets/02_Scripts/Recipe/RecipeUIManager.cs
@@ -7,13 +7,31 @@ public class RecipeUIManager : MonoBehaviour
 {
     [SerializeField] private Button recipeBookButton; // ·¹½ÃÇÇ ºÏ ¹öÆ°
     [SerializeField] private GameObject recipeBookImage; // ·¹½ÃÇÇ ºÏ ÀÌ¹ÌÁö
+    [SerializeField] private Button recipeBookCloseBtn; // ·¹½ÃÇÇ ºÏ ´Ý±â ¹öÆ°
+
     [SerializeField] private GameManager ball; // ¹Í½Ìº¼
 
     private void OnEnable()
     {
         recipeBookImage.SetActive(false);
 
-        recipeBookButton.onClick.AddListener(() => recipeBookImage.SetActive(!recipeBookImage.activeSelf));
+        recipeBookButton.onClick.AddListener(() =>
+        {
+            ActiveGameobject(recipeBookButton.gameObject);
+            ActiveGameobject(recipeBookImage);
+        });
+
+        recipeBookCloseBtn.onClick.AddListener(() =>
+        {
+            ActiveGameobject(recipeBookButton.gameObject);
+            ActiveGameobject(recipeBookImage);
+        });
+
+
     }
 
+    private void ActiveGameobject(GameObject go)
+    {
+        go.SetActive(!go.activeSelf);
+    }
 }
