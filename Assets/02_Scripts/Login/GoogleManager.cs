cbca2c10bc6a0eb267ca48ac080a0b46b1666697
juyeon0514 <sk651726@gmail.com>
2024-12-28 10:05:16 +0900

    feat:인벤토리 x버튼 추가
    - 인벤토리 X 버튼 추가
    - 로그인 통신 코드 추가

--- Assets/02_Scripts/Login/GoogleManager.cs
+++ Assets/02_Scripts/Login/GoogleManager.cs
@@ -4,21 +4,18 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEditor;
+using System.IO;
 
 public class GoogleManager : MonoBehaviour
 {
     public TextMeshProUGUI loginText;
+    public TextMeshProUGUI loginText2;
 
     private void Start()
     {
         PlayGamesPlatform.DebugLogEnabled = true;
         PlayGamesPlatform.Activate();
-        
-        SignIn();
-    }
-
-    public void SignIn()
-    {
         PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
     }
 
@@ -29,12 +26,44 @@ public class GoogleManager : MonoBehaviour
             string name = PlayGamesPlatform.Instance.GetUserDisplayName();
             string id = PlayGamesPlatform.Instance.GetUserId();
             string Imgurl = PlayGamesPlatform.Instance.GetUserImageUrl();
+            loginText2.text = name;
+        }
+        else if (status == SignInStatus.InternalError)
+        {
+            loginText.text = "error";
+        }
+        else if (status == SignInStatus.Canceled)
+        {
+            loginText.text = "cancle";
+        }
+        else 
+        {
+            loginText.text = "Login Fail2";
+        }
+    }
 
-            loginText.text = "Succes :" + name;
+    public void PushTest()
+    {
+        if (Social.localUser.authenticated)
+        {
+            PlayGamesPlatform.Instance.RequestServerSideAccess(false, code =>
+            {
+                if (!string.IsNullOrEmpty(code))
+                {
+                    loginText.text = "Code: " + code; // UI¿¡ Ç¥½Ã
+                    string path = Application.persistentDataPath + "/auth_code.txt";
+                    File.WriteAllText(path, "Code: " + code);
+                }
+                else
+                {
+                    loginText.text = "AuthoFail";
+                }
+            });
         }
         else
         {
-            loginText.text = "Login Fail";
+
+            loginText.text = "login Fail";
         }
     }
 }
