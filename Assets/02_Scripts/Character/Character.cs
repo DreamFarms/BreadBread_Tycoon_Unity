708da4594f66d7f13343a19c451a5f6d8597a215
Jihwan Byeon <wkdghk24@gmail.com>
2024-12-28 10:08:09 +0900

    fix : enum 오류 수정
    - namespace 충돌로 인한 오류
    - Ingredients를 IngredientEnum으로 변경
    - Bread를 BreadEnum으로 변경

---
+++ Assets/02_Scripts/Character/Character.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Bread
+{
+    Sandwich,
+    Icebox_Strawberry,
+    Donut_Chocolate,
+    ButterBar_Plane,
+    RollCake_Chocolate,
+    RollCake_Strawberry,
+    ButterRoll_Salted,
+    Bread_Melon,
+    Cake_Strawberry, // "Strawberr"가 아닌 "Strawberry"로 수정
+    Cookie_Strawberry,
+    DinnerRoll
+}
+
+public enum Ingredient
+{
+    Flour,
+    Flour_Green,
+    Flour_Red,
+    Salt,
+    Sugar,
+    Butter,
+    Egg,
+    Milk,
+    FreshApple,
+    FreshBanana,
+    FreshBlueberry,
+    FreshPeach,
+    FreshStrawberry,
+    Chocolate,
+    MelonSyrup,
+    Vanilla
+}
+
+
+
+public abstract class Character : MonoBehaviour
+{
+    private Stack pickedBreadStack = new Stack();
+    private List<Bread> preferBreads = new List<Bread>();
+    private List<Ingredient> preferIngredients = new List<Ingredient>();
+
+    // 선호하는 빵/재료인지 확인하는 메서드
+    public void CheckPrefer(string breadName)
+    {
+        foreach(var bread in preferBreads)
+        {
+            if(breadName.Equals(bread))
+            {
+                PickBread(breadName);
+                return;
+            }
+        }
+        Debug.Log($"{gameObject.name}은 매대에 선호하는 빵이 없으므로 아무것도 사지 않았습니다.");
+    }
+
+    // 빵을 하나씩 고르는 메서드
+    public void PickBread(string breadName)
+    {
+        // todo : 확률
+        pickedBreadStack.Push(breadName);
+    }
+
+    // 캐릭터 특성 설정하는 메서드
+    // 선호하는 빵, 선호하는 재료를 설정
+    public abstract void SetCharacterPrefer();
+}
